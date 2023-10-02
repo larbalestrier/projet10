dcda2debe4957716982dfc64a2be951c752edfdb
quentin l'arbalestrier <quentin.larbalestrier@gmail.com>
2023-10-02 13:01:50 +0200

    modification sur le placement de la collback on succes et résolution du test Form, réduction du setTimeout

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,7 +4,7 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 800); })
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -12,11 +12,11 @@ const Form = ({ onSuccess, onError }) => {
     async (evt) => {
       evt.preventDefault();
       setSending(true);
+      onSuccess()
       // We try to call mockContactApi
       try {
         await mockContactApi();
         setSending(false);
-        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
