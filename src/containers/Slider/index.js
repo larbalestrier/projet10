ab2bcf5452836f40400e64c173949531d72cdf9e
quentin l'arbalestrier <quentin.larbalestrier@gmail.com>
2023-10-06 15:40:52 +0200

    Réorganiser l'ordre des images du slider et des points du slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,7 +8,7 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
+    new Date(evtA.date) > new Date(evtB.date) ? 1 : -1
   );
   
   const nextCard = () => {
@@ -44,7 +44,7 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${radioIdx + 1}`}
+                key={`${radioIdx * 3}`}
                   type="radio"
                   name="radio-button"
                   checked={idx === radioIdx}
